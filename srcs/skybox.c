801f867611a33b14e6c66e1a4c5e65df7505a6b8
Edouard RUCQUOY <erucquoy@e2r8p7.42.fr>
2017-06-06 04:49:17 +0200

    skybox suite

--- srcs/skybox.c
+++ srcs/skybox.c
@@ -6,7 +6,7 @@
 /*   By: bmickael <bmickael@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/05 23:43:10 by bmickael          #+#    #+#             */
-/*   Updated: 2017/06/06 02:59:56 by erucquoy         ###   ########.fr       */
+/*   Updated: 2017/06/06 04:30:56 by erucquoy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ void			init_sky(t_env *e, char *file_name)
 	img_f = malloc(sizeof(t_bmp));
 	img_f->dim.x = ((WIDTH * 360) / 60);
 	img_f->dim.y = HEIGHT;
-	img_f->pix = malloc(img_f->dim.x * HEIGHT * 4);
+	img_f->pix = (int *)malloc(img_f->dim.x * HEIGHT * 4);
 	copy_img(img_f, sky_bmp);
 	e->sky.sky = img_f;
 	e->sky.ratio = 360 / 60;
@@ -68,6 +68,7 @@ void			move_sky(t_env *e, int direction)
 	j = 0;
 	h = 0;
 	i = e->sky.pos;
+	//i < 0 ? i +=
 //	ft_printf("New direction = %i\n", direction);
 	if (direction == 0)
 	{
@@ -80,11 +81,28 @@ void			move_sky(t_env *e, int direction)
 			if (i >= (e->sky.sky->dim.x * HEIGHT))
 				break;
 		}
-	}
-	if (direction == 0)
 		e->sky.pos += 10;
+	}
 	if (direction == 1)
-		e->sky.pos--;
+	{
+		while (j < (WIDTH * HEIGHT))
+		{
+			if (j > 0 && j % (WIDTH) == 0)
+				i = (++h * e->sky.ratio * WIDTH) + e->sky.pos;
+			if (j < 0 || i < 0)
+				ft_printf("j=%d i=%d\n", j, i);
+			/*while (i < 0)
+			{
+				//i += (e->sky.sky->dim.x * HEIGHT);
+				ft_printf("j=%d i=%d / sizeof pix %d\n", j, i, sizeof(e->sky.sky->pix));
+			}*/
+			e->img_string[j] = e->sky.sky->pix[i];
+			1 ? j++, i++ : (0);
+			if (i >= (e->sky.sky->dim.x * HEIGHT))
+				break;
+		}
+		e->sky.pos -= 10;
+	}
 }
 
 /*
